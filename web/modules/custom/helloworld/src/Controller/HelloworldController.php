9e789f299a25cc6e200c71dcb4d89626db5cb1c3
Gerardo Luis <gerardo.luismartinez@sasktel.com>
2026-01-08 20:40:38 +0000

    added new dynamic route for hello module

--- web/modules/custom/helloworld/src/Controller/HelloworldController.php
+++ web/modules/custom/helloworld/src/Controller/HelloworldController.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Drupal\helloworld\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
+use function kint;
 
 /**
  * Returns responses for Hello World routes.
@@ -12,14 +14,53 @@ use Drupal\Core\Controller\ControllerBase;
 final class HelloworldController extends ControllerBase {
 
 
-  public function hello(): array {
+  public function hello($name = NULL): array {
+
+    $output = $this ->t('Hello to my World');
+
+    if($name){
+      $output = $this->t('Hello @name', ['@name' => $name]);
+    }
 
     $build['content'] = [
       '#type' => 'item',
-      '#markup' => $this->t('Custom module!'),
+      '#markup' => $output,
     ];
 
     return $build;
+  }
+
+  /**
+   * Builds the response for hello_name_node
+   */
+  public function helloNameNode($name, $node_id): array {
+
+    $node = Node::load($node_id);
+
+    //dpm($node);
+    //dpm($node->title->value);
+    //dpm($node->getTitle());
+    //dpm($node->get('title')->value);
+    
+    if($node){
+      $output = $this->t('Hello @name! <br> The title of the node is @title.', [
+        '@name' => $name, 
+        '@title' => $node->getTitle(), 
+      ]);
+    }else{
+      $output = $this->t('Hello @name! <br> The node ID @id doesn\'t exist.', [
+        '@name' => $name, 
+        '@id' => $node_id, 
+      ]);
+    }
+
+    $build['content'] = [
+      '#type' => 'item',
+      '#markup' => $output,
+    ];
+
+    return $build;
+
   }
 
 }
